27c97711d1f0e3e2f50e4c9c9894f96eea8272b5
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:22 +0330

    committed by script

--- site/parts/targetProgress.jsx
+++ site/parts/targetProgress.jsx
@@ -1,5 +1,5 @@
-export default ({ snapshot }) => <dl class='targetProgress'>
-    <dt class='targetValue'>{snapshot.targetValue}</dt>
+export default ({ snapshot }) => <dl class='progress'>
+    <dt class='value'>{snapshot.targetValue}</dt>
     <dd class='achievedValue'>{snapshot.achievedValue}</dd>
     <dd class='attainmentPercentage'>{snapshot.attainmentPercentage}</dd>
 </dl>
