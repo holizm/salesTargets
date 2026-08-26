845d0ad8b8bbdd9239123f60325a364c4770d194
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:29:00 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='trackChanges'
+    name='salesTargets'
+    title='salesTargetsSalesTargets'
+    {...props}
+/>
